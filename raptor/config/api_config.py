74c3ff912b054c320db10da6a64e8458e409a319
Sumin0916 <gsmin2018@gmail.com>
2024-11-13 15:45:04 +0900

    api_config 내용 추가

--- raptor/config/api_config.py
+++ raptor/config/api_config.py
@@ -1,20 +1,38 @@
-CLOVA_HOST = 'clovastudio.apigw.ntruss.com'
-CLOVA_API_KEY = 'your-api-key'
-CLOVA_API_GATEWAY_KEY = 'your-gateway-key'
+# OPENAI_API
+OPENAI_API_KEY = "여기에_OPENAI_API_키_입력"
+
+# 하이퍼클로바
+CLOVA_HOST = 'clovastudio.apigw.ntruss.com'  # 고정
+CLOVA_API_KEY = '여기에_CLOVA_API_키_입력'
+CLOVA_API_GATEWAY_KEY = '여기에_CLOVA_GATEWAY_키_입력'
 
 # 요약 모델 설정
 SUMMARY_CONFIG = {
-    'request_id': 'your-summary-request-id',
-    'endpoint': '/testapp/v1/api-tools/summarization/v2/0190be7adfa24c7a9e10b1ac2edebd78'
+    'request_id': '여기에_REQUEST_ID_입력',
+    'endpoint': '/testapp/v1/api-tools/summarization/v2/e996884683d148c591bf295c7b4b8fb3'  # 고정
 }
 
 # 임베딩 모델 설정
 EMBEDDING_CONFIG = {
-    'request_id': 'your-embedding-request-id',
-    'endpoint': '/testapp/v1/api-tools/embedding/v2/a8893d2270834ab888ded5e01ff1eff1'
+    'request_id': '여기에_REQUEST_ID_입력',
+    'endpoint': '/testapp/v1/api-tools/embedding/clir-emb-dolphin/7437fa9facdf4658bcfc4c4d1cd4ebb5'  # 고정
+}
+
+EXTRACT_CONFIG = {
+    'request_id': '여기에_REQUEST_ID_입력',
+    'endpoint': '/testapp/v1/chat-completions/HCX-003'  # 고정
 }
 
 HCX_003_CONFIG = {
-    'request_id': 'your-hcx-request-id',
-    'endpoint': '/testapp/v1/chat-completions/HCX-003'
+    'request_id': '여기에_REQUEST_ID_입력',
+    'endpoint': '/testapp/v1/chat-completions/HCX-003'  # 고정
 }
+
+HCX_SEGMENTATION_CONFIG = {
+    'request_id': '여기에_REQUEST_ID_입력',
+    'endpoint': '/testapp/v1/api-tools/segmentation/818efcf6224c4eb39c967ebd910b3841'  # 고정
+}
+
+# 토큰 제한 설정
+HCX_EMBEDDING_MAX_TOKENS = 8191
+OPENAI_EMBEDDING_MAX_TOKENS = 8191
